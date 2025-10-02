62d30668b731c0f13a497dae288d8aa857cc12e7
rishisoyal <rishisoyal510@gmail.com>
2025-10-02 18:22:42 +0530

    correct: import typo

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import CustomCursor from "./Components/CustomCursor";
+import CustomCursor from "./components/CustomCursor";
 
 createRoot(document.getElementById("root")).render(
   <>
