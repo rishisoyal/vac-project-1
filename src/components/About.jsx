7cdc5f139825e1f76d76a185ad39474f9188756e
rishisoyal <rishisoyal510@gmail.com>
2025-10-05 16:32:24 +0530

    correct: change html class to react className

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,64 +9,64 @@ const About = () => {
           <h1>
             About <span className="highlight">Me</span>
           </h1>
-          <section class="personal-info-section">
-            <div class="personal-info">
+          <section className="personal-info-section">
+            <div className="personal-info">
               <h2>PERSONAL INFOS</h2>
-              <div class="info-grid">
-                <div class="info-item">
+              <div className="info-grid">
+                <div className="info-item">
                   <strong>First Name:</strong> Rishi
                 </div>
-                <div class="info-item">
+                <div className="info-item">
                   <strong>Last Name:</strong> Soyal
                 </div>
-                <div class="info-item">
+                <div className="info-item">
                   <strong>Age:</strong> 21 Years
                 </div>
-                <div class="info-item">
+                <div className="info-item">
                   <strong>Nationality:</strong> Indian
                 </div>
-                <div class="info-item">
+                <div className="info-item">
                   <strong>Freelance:</strong>{" "}
-                  <span class="available">Available</span>
+                  <span className="available">Available</span>
                 </div>
-                <div class="info-item">
+                <div className="info-item">
                   <strong>Address:</strong> Cool Place
                 </div>
-                <div class="info-item">
+                <div className="info-item">
                   <strong>Phone:</strong> +911234567890
                 </div>
-                <div class="info-item">
+                <div className="info-item">
                   <strong>Email:</strong> cooldude@mail.com
                 </div>
-                <div class="info-item">
+                <div className="info-item">
                   <strong>GitHub:</strong> rishisoyal
                 </div>
-                <div class="info-item">
+                <div className="info-item">
                   <strong>Languages:</strong> Hindi, English
                 </div>
               </div>
-              <a href="#" class="download-cv">
+              <a href="#" className="download-cv">
                 <span>DOWNLOAD CV</span>
-                <div class="download-icon">⬇</div>
+                <div className="download-icon">⬇</div>
               </a>
             </div>
 
-            <div class="stats">
-              <div class="stat-box">
-                <span class="stat-number">12+</span>
-                <span class="stat-text">YEARS OF EXPERIENCE</span>
+            <div className="stats">
+              <div className="stat-box">
+                <span className="stat-number">12+</span>
+                <span className="stat-text">YEARS OF EXPERIENCE</span>
               </div>
-              <div class="stat-box">
-                <span class="stat-number">97+</span>
-                <span class="stat-text">COMPLETED PROJECTS</span>
+              <div className="stat-box">
+                <span className="stat-number">97+</span>
+                <span className="stat-text">COMPLETED PROJECTS</span>
               </div>
-              <div class="stat-box">
-                <span class="stat-number">81+</span>
-                <span class="stat-text">HAPPY CUSTOMERS</span>
+              <div className="stat-box">
+                <span className="stat-number">81+</span>
+                <span className="stat-text">HAPPY CUSTOMERS</span>
               </div>
-              <div class="stat-box">
-                <span class="stat-number">53+</span>
-                <span class="stat-text">AWARDS WON</span>
+              <div className="stat-box">
+                <span className="stat-number">53+</span>
+                <span className="stat-text">AWARDS WON</span>
               </div>
             </div>
           </section>
